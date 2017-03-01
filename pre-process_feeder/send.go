4a2a9d570389f453a6339c16fef0060150f4b466
Brian Shaw <bshaw@vsvinc.com>
2017-02-28 19:42:17 -0500

    initial commit

---
+++ pre-process_feeder/send.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"log"
+	"github.com/streadway/amqp"
+	"io/ioutil"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"pre-process", // name
+		true,    // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	files, _ := ioutil.ReadDir("/home/bshaw/rpmbuild/SPECS")
+	for _, f := range files {
+		body := f.Name()
+		err = ch.Publish(
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		log.Printf(" [x] Sent %s", body)
+		failOnError(err, "Failed to publish a message")
+	}
+}
